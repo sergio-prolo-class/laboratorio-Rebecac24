e919e691e29eaa0c343a4cb78a81fd0d2223473f
Rebecac24 <rebeca.c24@aluno.ifsc.edu.br>
2025-05-21 14:28:57 +0000

    exercicios caixas

---
+++ aulas/aula 21/caixas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float altura;
+    float largura;
+    float profundidade;
+    float area;
+    float volume;
+    printf("Digite a altura (m)");
+    scanf("%f", &altura);
+    printf("Digite a largura (m)");
+    scanf("%f", &largura);
+    printf("Digite a profundidade (m)");
+    scanf("%f", &largura);
+    return 0;
+}
