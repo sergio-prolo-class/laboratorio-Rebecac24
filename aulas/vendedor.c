97a2165ea853b1a3763e07f5e1b024458a1bb138
Rebecac24 <rebeca.c24@aluno.ifsc.edu.br>
2025-06-04 14:11:22 +0000

    calculadora

--- aulas/vendedor.c
+++ aulas/vendedor.c
@@ -3,5 +3,16 @@
 int main()
 {
     // Variáveis
-    float mensal, fixo, comissão, meta, desempenho, bonus
+    float vendas, salario, comissao;
+    int desempenho;
+    salario = 20000;
+    printf("Entre com o total de vendas: ");
+    scanf(" %f", &vendas);
+    printf("Escreva a avaliação de desempenho(1-10):");
+    scanf(" %d", &desempenho);
+    if (vendas >= 10000)
+        ;
+    {
+        comissao = vendas * 0.07;
+    }
 }
