2138e30b091b5f6b85ff63ef5ce14633f6ffa00b
Rebecac24 <rebeca.c24@aluno.ifsc.edu.br>
2025-06-11 14:24:13 +0000

    aula

---
+++ aulas/haverseno.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float hav;
+    {
+        float resultado;
+    }
+    float graus2rad(float graus);
+    float distancia_na_terra(float lat1, float lon1, float lat2, float lon2);
+}
