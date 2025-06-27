3f511a5bec4c5f97b1642a5d4eaca246edaa94cc
Rebecac24 <rebeca.c24@aluno.ifsc.edu.br>
2025-06-27 10:47:47 +0000

    .

---
+++ aulas/estatisticas.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct {
+    float media, min, max;
+} Estatisticas;
+
+Estatisticas calcula_estatisticas(int n, float e[])
+{
+    float stats = {0, INFINITY, -INFINITY};
+    for (int = 0; i < n; i++) {
+        stats.media += e[i];
+    }
+}
+
+int main()
+{
+    int numero;
+    float elementos[numero];
+
+    printf("Entre com o número de elementos (máx 100): ");
+    scanf("%d", numero);
+
+    float elementos[numero];
+
+    printf("Entre com os elementos: ");
+    for (int i = 0; i < numero, i++)
+
+        return EXIT_SUCCESS;
+}
